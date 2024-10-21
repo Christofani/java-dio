d33352907387f5df12c690db7f72c485b83b79a7
Rodrigo Christofani <christofanir@gmail.com>
2024-10-21 10:44:42 -0300

    Operadores concluído.

---
+++ operadores/operadores/src/main/java/org/dio/src/Operadores3.java
@@ -0,0 +1,17 @@
+package org.dio.src;
+
+public class Operadores3 {
+
+  public static void main(String[] args) {
+    int numero = 5;
+
+    numero++;
+    System.out.println(numero);
+
+    boolean variavel = true;
+
+    variavel = !variavel;
+
+    System.out.println(variavel);
+  }
+}
