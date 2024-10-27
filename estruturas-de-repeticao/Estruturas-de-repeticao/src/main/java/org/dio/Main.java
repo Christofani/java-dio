dce7d7156d1a1853264ed03133adbe810a9dfb61
Rodrigo Christofani <christofanir@gmail.com>
2024-10-27 13:57:16 -0300

    Exemplo de While e Do While.

--- estruturas-de-repeticao/Estruturas-de-repeticao/src/main/java/org/dio/Main.java
+++ estruturas-de-repeticao/Estruturas-de-repeticao/src/main/java/org/dio/Main.java
@@ -2,6 +2,7 @@ package org.dio;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    String[] nomes = {"Camila", "Venilton", "Leonardo", "Renan", "Rafael"};
+    System.out.print(nomes.length);
   }
 }
